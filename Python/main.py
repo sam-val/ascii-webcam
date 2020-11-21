132ef53d9e3ba39e171ed7fee28529e73d6f2e41
Sam Val <samvalentine2811@gmail.com>
2020-11-21 11:50:28 +0700

    minor variable name-changes

--- Python/main.py
+++ Python/main.py
@@ -9,7 +9,7 @@ def test_cam(cam):
     return True
 
 def string_to_image_data(str, width, height):
-    ### find the largest character's width, aka the '@' char:
+    # find the largest character's width, aka the '@' char:
     (max_char_w, max_char_h), base_line = cv2.getTextSize("@", font, font_scale, font_thickness)
 
     # control the new line height:
@@ -19,9 +19,9 @@ def string_to_image_data(str, width, height):
     img = np.zeros((int(new_line_distance*height),max_char_w*width), np.uint8)
 
     # iterate and draw per character:
-    for i in range(height):
-        for j in range(width):
-            cv2.putText(img,str[i*width+j],(int(j*max_char_w),int(i*new_line_distance)),font,font_scale,255,font_thickness)
+    for y in range(height):
+        for x in range(width):
+            cv2.putText(img,str[y*width + x],(int(x*max_char_w),int(y*new_line_distance)),font,font_scale,255,font_thickness)
 
     return img
 
@@ -44,7 +44,7 @@ font_scale = 1
 font_thickness = 1
 
 # SET UP FRAME SETTINGS - WIDTH, HEIGHT, ETC:
-    # Check the camera:
+    ## Check the camera:
 CAM = 0
 print('Trying to access CAM', CAM)
 assert test_cam(CAM), f"Failed to access CAMERA {CAM}. Please Check you camera."
@@ -53,8 +53,8 @@ print("Accessed camera successfully.")
 capture = cv2.VideoCapture(CAM)
 width, height = 1200, 900
 RESIZE_AMOUNT = 15 # times
-    # Resize the frame to smaller one -- so that you can extract ASCII characters from it,
-    # otherwise - frame would be too big -- Ought to get width,height under (100,100)
+    ## Resize the frame to smaller one -- so that you can extract ASCII characters from it,
+    ## otherwise - frame would be too big -- Ought to get width,height under (100,100)
 width, height = width // RESIZE_AMOUNT, height // RESIZE_AMOUNT
 capture.set(3, width)  ## set '3': the width
 capture.set(4, height)  ## set '4': the height
